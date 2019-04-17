fe183763cf886fa19ead34fbe4dfcffe9b7f5f03
Elizabeth Aitken <Liz@Lizs-MacBook-Pro.local>
2019-04-17 07:34:43 -0400

    Added initial python script

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+noun = input("Noun: ")
+adjective = input("Adjective: ")
+verb = input("Verb: ")
+adverb = input("Adverb: ")
+
+print("Hello! I am a %s %s. I love to %s %s." %(adjective, noun, verb, adverb))
